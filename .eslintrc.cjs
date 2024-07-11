a3d0d7aca9172bd5112485e613dce26b7dee74d4
MHendriF <hendrifebriansyah28@gmail.com>
2024-07-11 19:04:18 +0700

    recreate app

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "prettier/prettier": ["error", { singleQuote: true }],
+  },
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { ThemeProvider } from './context/ThemeContext';
-import { LocaleProvider } from './context/LocaleContext';
-import Root from './Root';
-import RequireAuth from './RequireAuth';
-import LoginPage from './pages/login';
-import RegisterPage from './pages/register';
-import NotePage from './pages/note';
-import DetailNotePage from './pages/detailNote';
-import ArchivePage from './pages/archive';
-import ProfilePage from './pages/profile';
-import ErrorPage from './pages/error404';
-import CreateNotePage from './pages/createNote';
-import './index.css';
-
-const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path='/' element={<Root />} errorElement={<ErrorPage />}>
-            <Route path='login' element={<LoginPage />} />
-            <Route path='register' element={<RegisterPage />} />
-
-            <Route element={<RequireAuth />}>
-                <Route index element={<NotePage />} />
-                <Route path='notes/create' element={<CreateNotePage />} />
-                <Route path='notes/:id' element={<DetailNotePage />} />
-                <Route path='archives' element={<ArchivePage />} />
-                <Route path='profile' element={<ProfilePage />} />
-            </Route>
-        </Route>,
-    ),
-);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <ThemeProvider>
-                <LocaleProvider>
-                    <RouterProvider router={router} />
-                </LocaleProvider>
-            </ThemeProvider>
-        </React.StrictMode>
-    </Provider>,
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
